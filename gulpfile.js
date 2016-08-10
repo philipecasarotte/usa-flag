61172033e91ac940fa02c1073a4a7b00bbc2daba
Philipe Casarotte <philipe@dburnsdesign.com>
2016-08-10 14:30:46 -0700

    css  animations

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var connect = require('gulp-connect')
 var browserify = require('browserify')
 var source = require('vinyl-source-stream')
 
-// Connect task
 gulp.task('connect', function () {
 	connect.server({
 		root: 'public',
@@ -13,21 +12,19 @@ gulp.task('connect', function () {
 })
 
 gulp.task('browserify', function() {
-	// Grabs the app.js file
-    return browserify('./app/app.js')
+	return browserify('./app/app.js')
 		.bundle()
 		.pipe(source('main.js'))
-		.pipe(gulp.dest('./public/js/'));
+		.pipe(gulp.dest('./public/js/'))
 })
 
 gulp.task('sass', function() {
 	return sass('./sass/style.sass')
-		.pipe(gulp.dest('./public/css'));
+		.pipe(gulp.dest('./public/css'))
 })
 
 gulp.task('watch', function() {
 	gulp.watch('app/**/*.js', ['browserify'])
-	// Watches for changes in style.sass and runs the sass task
 	gulp.watch('sass/style.sass', ['sass'])
 })
 
